b5aad629e3e27873c6969014c1424db36464045d
Michael Staehling <Michael.staehling@gmail.com>
2015-07-09 16:54:57 -0400

    set up routes

--- app/scripts/route.js
+++ app/scripts/route.js
@@ -7,13 +7,25 @@
 
   function routeConfig($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'app/index.html',
-        controller: 'MainController',
-        controllerAs: 'main'
+      .when('/home', {
+        templateUrl: 'app/views/home.html'
+        // controller: 'MainController',
+        // controllerAs: 'main'
+      })
+      .when('/submit', {
+        templateUrl: 'app/views/submit.html'
+      })
+      .when('/stops', {
+        templateUrl: 'app/views/stops.html'
+      })
+      .when('/search', {
+        templateUrl: 'app/views/search.html'
+      })
+      .when('/sign_up', {
+        templateUrl: 'app/views/sign_up.html'
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/home'
       });
   }
 

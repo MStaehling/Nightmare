825ba829fba237c295e22ea36aaefdac668bd8e0
Michael Staehling <Michael.staehling@gmail.com>
2015-07-14 14:58:08 -0400

    cleaned up the js file

--- app/scripts/stop.js
+++ app/scripts/stop.js
@@ -4,13 +4,9 @@
 
   angular.module('nightmare')
     .controller('SingleController', function($scope, $http) {
-      var list = this;
-      list.stop = [];
       $http.get('https://fiery-inferno-4540.firebaseio.com/.json')
         .then(function(response) {
-          console.log(response.data);
           $scope.stop = response.data;
-          // console.log(list.stop);
         })
     })
 

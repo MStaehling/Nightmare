75271a5eb0d85921538fc01cf744ca0980a833c4
Michael Staehling <Michael.staehling@gmail.com>
2015-07-16 17:19:06 -0400

    cleaned up submit.js and working on getting comments to post

--- app/scripts/stop.js
+++ app/scripts/stop.js
@@ -3,13 +3,42 @@
   'use strict';
 
   angular.module('nightmare')
-    .controller('StopController', function($scope, $http, $routeParams){
-      // $scope.id = $routeParams.$id;
+    .controller('StopController', function($scope, $http, $routeParams, $firebaseArray){
+      // $scope.comment = {
+      //   comment: ''
+      // }
+
+
       $http.get('https://fiery-inferno-4540.firebaseio.com/' + $routeParams.stop + '.json')
       .then(function(response) {
         $scope.stop = response.data;
         console.log('hi', response);
       });
+
+      // var ref = new Firebase('https://fiery-inferno-4540.firebaseio.com' + $routeParams.stop + '/content');
+      // $scope.stop = $firebaseArray(ref);
+
+  $scope.submit = function() {
+    console.log($scope.stop);
+        $scope.stop.$add($scope.comment);
+        $scope.comment = "";
+      };
+
     });
+    // .controller('CommentController', function($scope, $http, $routeParams, $firebaseArray){
+    //   $scope.comment = {
+    //     comment: ''
+    //   };
+    //
+    //   var ref = new Firebase('https://fiery-inferno-4540.firebaseio.com' + $routeParams.stop + '/content');
+    //   $scope.stop = $firebaseArray(ref);
+    //
+    //   $scope.submit = function() {
+    //     console.log($scope.stop);
+    //         $scope.stop.$add($scope.comment);
+    //         $scope.comment = "";
+    //       };
+
+    //})
 
 })();
--- app/scripts/submit.js
+++ app/scripts/submit.js
@@ -50,19 +50,6 @@
 
     };
     console.log($scope.list);
-  })
-
-  // angular
-  //   .module('nightmare')
-    .controller('CommentController', function() {
-      this.submit = {};
-
-      this.addSubmit = function(stop) {
-        stop.submits.push(this.submit);
-        console.log('hi');
-        console.log(this.submit);
-        this.submit = {};
-      };
-    });
+  });
 
 })();

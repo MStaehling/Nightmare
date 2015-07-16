efed3a7f4d74039f23d8873d4314fe9cb4340f91
Michael Staehling <Michael.staehling@gmail.com>
2015-07-15 21:29:46 -0400

    cleaning up the stop.js

--- app/scripts/stop.js
+++ app/scripts/stop.js
@@ -5,10 +5,9 @@
   angular.module('nightmare')
     .controller('StopController', function($scope, $http, $routeParams){
       // $scope.id = $routeParams.$id;
-      console.log("hello");
       $http.get('https://fiery-inferno-4540.firebaseio.com/' + $routeParams.stop + '.json')
       .then(function(response) {
-        $scope.stop = response.data;//$routeParams.id
+        $scope.stop = response.data;
         console.log('hi', response);
       });
     });

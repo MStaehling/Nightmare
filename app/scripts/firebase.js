a71a2237c0333678074738b7c9673d8839b4b732
Michael Staehling <Michael.staehling@gmail.com>
2015-07-17 18:55:23 -0400

    got search bar to react to enter and go to search page when enter is pressed and only enter is pressed

--- app/scripts/firebase.js
+++ app/scripts/firebase.js
@@ -1,16 +1,25 @@
 /* global angular */
 
-// (function() {
-//   'use strict';
-//
-//   angular.module('nightmare')
-//
-//   .controller('MainController', function($scope) {
-//     $scope.enter = function(keyEvent) {
-//       if (keyEvent.which === 13)
-//         alert('I am an alert');
-//     };
-//
-//
-//   });
-// })();
+(function() {
+  'use strict';
+
+  angular.module('nightmare')
+
+  .directive('ngEnter', function($location) {
+    return function(scope, element, attrs) {
+      element.bind("keydown keypress", function(event) {
+        if (event.which === 13) {
+          scope.$apply(function() {
+            scope.$eval(attrs.ngEnter);
+            $location.url('/search');
+            console.log('hi');
+          });
+
+          event.preventDefault();
+        }
+      });
+    };
+  })
+
+  // got most of this from http://eric.sau.pe/angularjs-detect-enter-key-ngenter/
+})();

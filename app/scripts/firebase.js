6ddca453d0d077f186b5e51acb0985b48d43cc15
Michael Staehling <Michael.staehling@gmail.com>
2015-07-17 17:31:01 -0400

    reposition scripts

--- app/scripts/firebase.js
+++ app/scripts/firebase.js
@@ -1,4 +1,4 @@
-
+/* global angular */
 
 // (function() {
 //   'use strict';
@@ -6,20 +6,10 @@
 //   angular.module('nightmare')
 //
 //   .controller('MainController', function($scope) {
-//     var firebase = new Firebase('https://fiery-inferno-4540.firebaseio.com');
-//
-//     var self = this;
-//     // Above function to be used when templating -- commented out for `eslint` build for release 0.2.0
-//
-//     // this.login = function(){
-//     //   firebase.authWithOAuthPopup('github', function(error, auth){
-//     //     $scope.$apply(function(){
-//     //     self.username = auth.github.username;
-//     //       console.log('Its alive!');
-//     //
-//     //     });
-//     //   });
-//     // };
+//     $scope.enter = function(keyEvent) {
+//       if (keyEvent.which === 13)
+//         alert('I am an alert');
+//     };
 //
 //
 //   });

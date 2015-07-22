2a58e5e59c5878440068985c433f80e2f35d4519
Michael Staehling <Michael.staehling@gmail.com>
2015-07-22 09:58:24 -0400

    able to pick out the correct end point that I need to change just need to change and save it to that same end point

--- app/scripts/stop.js
+++ app/scripts/stop.js
@@ -3,30 +3,69 @@
   'use strict';
 
   angular.module('nightmare')
-    .controller('StopController', function($http, $routeParams, $firebaseArray){
+    .controller('StopController', function($http, $routeParams, $firebaseArray, $firebaseObject) {
 
       this.stop = {};
       var self = this;
 
-      function pullData(){
+      function pullData() {
         $http.get('https://fiery-inferno-4540.firebaseio.com/' + $routeParams.stop + '.json')
-        .then(function(response) {
-          self.stop = response.data;
-          console.log('hi', response);
-        });
+          .then(function(response) {
+            self.stop = response.data;
+            console.log('hi', response);
+          });
       }
 
       var ref = new Firebase('https://fiery-inferno-4540.firebaseio.com/' + $routeParams.stop + '/comments');
       self.stopComment = $firebaseArray(ref);
-
-  self.submit = function() {
-    console.log(self.stop);
+      console.log(self.stopComment);
+      self.submit = function() {
+        console.log(self.stop);
         self.stopComment.$add({
           comment: self.comment
         });
         self.comment = '';
         pullData();
       };
+
+      self.editComment = function(endpoint) {
+        var commentEndpoint = new Firebase('https://fiery-inferno-4540.firebaseio.com/' + $routeParams.stop + '/comments/' + endpoint);
+        self.indComment = $firebaseObject(commentEndpoint);
+        console.log('endpoint', endpoint);
+        console.log('hi', self.indComment);
+        self.indComment.comment = '';
+        console.log('hi', self.indComment);
+        self.indComment.$add({
+          comment: self.editedComment.comment
+        });
+        self.indComment = '';
+      //  self.indComment.$add(self.editedComment);
+        self.editing = false;
+        self.editedComment = {};
+      };
+
+      this.editing = false;
+      this.editedComment = {};
+
+      self.inputField = function() {
+        return self.editing = true;
+      };
+
+      // self.doneEditing = function(edit) {
+      //   self.editedComment = null;
+      //   var title = edit.comments.trim();
+      //   if (title) {
+      //     self.comments.$save(edit);
+      //   } else {
+      //     self.removeComment(edit);
+      //   }
+      // };
+      //
+      // self.revertEditing = function(edit) {
+      //   edit.comment = self.originalComment.title;
+      //   self.doneEditing(edit);
+      // };
+
       pullData();
     });
 })();
